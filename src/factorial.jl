ed1c505c42ca9a1e0e912f1f98d55c256ce4a031
Ellian Carlos <17656878+EllianCarlos@users.noreply.github.com>
2021-08-15 00:30:17 -0300

    chore: aplica estilo

--- src/factorial.jl
+++ src/factorial.jl
@@ -1,13 +1,25 @@
-caching = [1]
+caching_fac = [1]
 function fac(n)
     if n == 0
         return 1
-    elseif size(caching, 1) >= n
-        return caching[n]
+    elseif size(caching_fac, 1) >= n
+        return caching_fac[n]
     end
-    fac = caching[size(caching, 1)]
-    for i = size(caching, 1)+1:n
-        append!(caching, fac*i)
+    fac = caching_fac[size(caching_fac, 1)]
+    for i = size(caching_fac, 1)+1:n
+        append!(caching_fac, fac * i)
     end
-    return caching[n]
+    return caching_fac[n]
 end
+
+function stirlingNumber(m, k)
+    if k > m || k < 0 || (m > 0 && k == 0)
+        return 0
+    elseif m == 0 && k == 0
+        return 1
+    else
+        return k * stirlingNumber(m - 1, k) + stirlingNumber(m - 1, k - 1)
+    end
+end
+
+print(stirlingNumber(4, 4))
